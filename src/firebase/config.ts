570512977ffa4c59ca69b41edab5cc6424203aaf
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-14 05:58:59 +0000

    serafim.8790@hotmail.com Não foi possível fazer login, porque esse app e

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   "appId": "1:344820186473:web:3d53cee9849eb2ef9211f8",
   "storageBucket": "cadastro-3c63f.appspot.com",
   "apiKey": "AIzaSyDDRF22vXkiHT0T1NLWuqfj66xJ7TaPsDo",
-  "authDomain": "cadastro-3c63f-e1d52.firebaseapp.com",
+  "authDomain": "cadastro-3c63f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "344820186473"
 };
