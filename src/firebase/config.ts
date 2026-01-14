6871615df8111bf4a8a93998908f6ea233ce5fd2
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-14 05:53:41 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,11 @@
+
 export const firebaseConfig = {
   "projectId": "cadastro-3c63f",
   "appId": "1:344820186473:web:3d53cee9849eb2ef9211f8",
+  "storageBucket": "cadastro-3c63f.appspot.com",
   "apiKey": "AIzaSyDDRF22vXkiHT0T1NLWuqfj66xJ7TaPsDo",
   "authDomain": "cadastro-3c63f-e1d52.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "344820186473"
 };
+
