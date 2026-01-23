4e731ef14d38f67d324eedce3bb5e7084442fc3e
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-23 11:20:36 +0000

    Não estou conseguindo nem editar nem apagar os dados cadastrados, favor

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -1,2 +1,2 @@
-export const authImageUrl = "https://source.unsplash.com/1200x1800/?sunny,beach";
-export const tentBannerUrl = "https://source.unsplash.com/1600x600/?beach,umbrella";
+export const authImageUrl = "https://images.unsplash.com/photo-1506929562872-bb421503ef21?q=80&w=1968&auto=format&fit=crop";
+export const tentBannerUrl = "https://images.unsplash.com/photo-1507525428034-b723a9ce6890?q=80&w=2070&auto=format&fit=crop";
