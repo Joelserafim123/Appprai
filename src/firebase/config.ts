2b54d49e32ac7b330ae976d85c451bf2ece2b62c
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-28 12:50:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
   "projectId": "cadastro-3c63f",
-  "appId": "1:344820186473:web:68edb8061af3e0159211f8",
-  "apiKey": "AIzaSyD3I4LHBJbHD-Gqtc2HjhAoS-6gs6yI0HI",
+  "appId": "1:344820186473:web:85f26fc2a31c46979211f8",
+  "apiKey": "AIzaSyDOzKQMf4lfuf8m2qJ2PxVd94AT14l8gBw",
   "authDomain": "cadastro-3c63f-e1d52.firebaseapp.com",
-  "storageBucket": "cadastro-3c63f.appspot.com",
-  "measurementId": "",
+  "measurementId": "G-N8WQZ49JVG",
   "messagingSenderId": "344820186473"
 };
