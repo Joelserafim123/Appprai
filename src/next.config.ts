9608a1915f15c1ae62ed68ebe3b5862c43661e5f
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-29 05:24:44 +0000

    Ocorreu um erro no build Ocorreu um erro no processo de build ou lançame

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/assetlinks.json',
+        destination: '/api/assetlinks',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
