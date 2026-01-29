0f33a0a90e5a9dfd8a21de1fe6fe549bc4f885a3
Fuel coin nova Criptomoeda moeda <serafim.8790@hotmail.com>
2026-01-29 13:23:43 +0000

    Erro ao Salvar
    Firebase Storage: No default bucket found. Did you set t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:344820186473:web:85f26fc2a31c46979211f8",
   "apiKey": "AIzaSyDOzKQMf4lfuf8m2qJ2PxVd94AT14l8gBw",
   "authDomain": "cadastro-3c63f-e1d52.firebaseapp.com",
+  "storageBucket": "cadastro-3c63f.appspot.com",
   "measurementId": "G-N8WQZ49JVG",
   "messagingSenderId": "344820186473"
 };
